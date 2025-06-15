726f38deb82484edb8c393c9617170f9445c3c70
dEpayse <cbs5295@gmail.com>
2025-06-15 11:20:37 +0900

    feat: name.ts 파일 추가 및 변수 정의

---
+++ src/app/constants/name.ts
@@ -0,0 +1,8 @@
+export const groomFirstName: string = "범순";
+export const brideFirstName: string = "승주";
+
+export const groomLastName: string = "최";
+export const brideLastName: string = "류";
+
+export const groomFullName: string = `${groomLastName}${groomFirstName}`
+export const brideFullName: string = `${brideLastName}${brideFirstName}`
