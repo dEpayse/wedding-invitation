e7828227f849f02596335ce418da4e846e463620
dEpayse <cbs5295@gmail.com>
2025-06-15 13:59:23 +0900

    feat: 가족 정보 이름 추가

--- src/app/constants/name.ts
+++ src/app/constants/name.ts
@@ -4,8 +4,23 @@ export const brideFirstName: string = "승주";
 export const groomLastName: string = "최";
 export const brideLastName: string = "류";
 
-export const groomFullName: string = `${groomLastName}${groomFirstName}`
-export const brideFullName: string = `${brideLastName}${brideFirstName}`
+export const groomFullName: string = `${groomLastName}${groomFirstName}`;
+export const brideFullName: string = `${brideLastName}${brideFirstName}`;
 
-export const groomFirstNameEn: string = "BEOMSOON"
-export const brideFirstNameEn: string = "SEUNGJU"
+export const groomFirstNameEn: string = "BEOMSOON";
+export const brideFirstNameEn: string = "SEUNGJU";
+
+export const groomFatherFirstName: string = "종해";
+export const groomMotherFirstName: string = "명선";
+
+export const brideFatherFirstName: string = "석현"
+export const brideMotherFirstName: string = "소영"
+
+export const groomMotherLastName: string = "박";
+export const brideMotherLastName: string = "임";
+
+export const groomFatherFullName: string = `${groomLastName}${groomFatherFirstName}`;
+export const groomMotherFullName: string = `${groomMotherLastName}${groomMotherFirstName}`;
+
+export const brideFatherFullName: string = `${brideLastName}${brideFatherFirstName}`;
+export const brideMotherFullName: string = `${brideMotherLastName}${brideMotherFirstName}`;
