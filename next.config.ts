59098ec03ae925dabb29fb0f22ee60bc9240ddfb
depayse <cbs5295@gmail.com>
2025-10-12 09:42:55 +0900

    feat. assetPrefix 추가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: '/wedding-invitation',
+  assetPrefix: '/wedding-invitation',
 };
 
 export default nextConfig;
