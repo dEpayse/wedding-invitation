4aa2f0253f5bf45422f1feedf019380c51945952
dEpayse <cbs5295@gmail.com>
2025-11-02 10:05:07 +0900

    feat. 커버 작성 완성

--- src/app/components/TopContent.tsx
+++ src/app/components/TopContent.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
 import styles from "./TopContent.module.css";
 import {brideFirstName, brideFirstNameEn, groomFirstName, groomFirstNameEn} from "@/app/constants/name";
-import {WEDDING_CONVENTION_NAME} from "@/app/constants/wedding";
 
 export default function TopContent() {
   return (
     <div className={styles.topContent}>
-      <div className={styles.imageWrapper}>
+      <p className={styles.imageWrapper}>
         <Image
           src="/cover_image.png"
           alt={`${groomFirstName}과 ${brideFirstName}의 결혼식`}
@@ -15,12 +14,21 @@ export default function TopContent() {
           height={300}
           priority
         />
-        <div className={styles.textOverlay}>
-          <h1 className={styles.title}>{groomFirstNameEn} & {brideFirstNameEn}</h1>
-          <p className={styles.subtitle}>2026년 2월 7일 토요일 오후 4시</p>
-          <p className={styles.subtitle}>{WEDDING_CONVENTION_NAME}</p>
+
+        {/* 제목 */}
+        <div className={styles.title}>
+          <p>
+            We are
+            <br />
+            getting
+            <br />
+            married
+          </p>
         </div>
-      </div>
+        <div className={styles.textOverlay}>
+          <p className={styles.names}>{brideFirstNameEn} AND {groomFirstNameEn}</p>
+        </div>
+      </p>
     </div>
   );
 }
