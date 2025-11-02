52ea4064e3ce7ec248574f9dd25c3f0f74fc560a
dEpayse <cbs5295@gmail.com>
2025-11-02 18:04:32 +0900

    feat. 가족 정보 추가

--- src/app/components/family/FamilyInfos.tsx
+++ src/app/components/family/FamilyInfos.tsx
@@ -1,89 +1,80 @@
-import styles from './FamilyInfos.module.css';
+import styles from "./FamilyInfos.module.css";
 import {
-    brideFatherFullName, brideFirstName,
-    brideMotherFullName,
-    groomFatherFullName,
-    groomFirstName,
-    groomMotherFullName
+  brideFatherFullName,
+  brideFirstName,
+  brideMotherFullName,
+  groomFatherFullName,
+  groomFirstName,
+  groomMotherFullName,
 } from "@/app/constants/name";
 import VerticalSpacer from "@/app/components/common/VerticalSpacer";
-import Button from "@/app/components/button/Button";
-import Image from "next/image";
+import HorizontalSpacer from "../common/HorizontalSpacer";
 
 export function FamilyInfos() {
-    return (
-        <div className={styles.container}>
-            <VerticalSpacer size={80}/>
-            <FamilyInfo
-                fatherFullName={groomFatherFullName}
-                motherFullName={groomMotherFullName}
-                relation={`의 아들`}
-                groomOrBrideStr={`신랑`}
-                groomOrBrideFirstName={groomFirstName}/>
-            <VerticalSpacer size={32}/>
-            <div className={styles.divider} />
-            <VerticalSpacer size={32}/>
-            <FamilyInfo
-                fatherFullName={brideFatherFullName}
-                motherFullName={brideMotherFullName}
-                relation={`의 딸`}
-                groomOrBrideStr={`신부`}
-                groomOrBrideFirstName={brideFirstName}/>
-            <VerticalSpacer size={48}/>
-            <Button className={styles.congratulations}>
-                <div className={styles.congratulationsDiv}>
-                    <span>축하 연락하기</span>
-                    <Image
-                        width={25}
-                        height={25}
-                        src="/ic_arrow_right.svg"
-                        alt="화살표"/>
-                </div>
-            </Button>
-            <VerticalSpacer size={80}/>
-        </div>
-    )
+  return (
+    <div className={styles.container}>
+      <VerticalSpacer size={120} />
+      <FamilyInfo
+        fatherFullName={groomFatherFullName}
+        motherFullName={groomMotherFullName}
+        relation={`의 아들`}
+        groomOrBrideStr={`신랑`}
+        groomOrBrideFirstName={groomFirstName}
+      />
+      <VerticalSpacer size={32} />
+      <div className={styles.divider} />
+      <VerticalSpacer size={32} />
+      <FamilyInfo
+        fatherFullName={brideFatherFullName}
+        motherFullName={brideMotherFullName}
+        relation={`의 딸`}
+        groomOrBrideStr={`신부`}
+        groomOrBrideFirstName={brideFirstName}
+      />
+      <VerticalSpacer size={120} />
+    </div>
+  );
 }
 
 interface FamilyInfo {
-    fatherFullName: string;
-    motherFullName: string;
-    relation: string;
-    groomOrBrideStr: string;
-    groomOrBrideFirstName: string;
+  fatherFullName: string;
+  motherFullName: string;
+  relation: string;
+  groomOrBrideStr: string;
+  groomOrBrideFirstName: string;
 }
 
-function FamilyInfo(
-    {
-        fatherFullName,
-        motherFullName,
-        relation,
-        groomOrBrideStr,
-        groomOrBrideFirstName,
-    } : FamilyInfo
-) {
-    return (
+function FamilyInfo({
+  fatherFullName,
+  motherFullName,
+  relation,
+  groomOrBrideStr,
+  groomOrBrideFirstName,
+}: FamilyInfo) {
+  return (
+    <div>
+      <div className={styles.familyFirstRow}>
         <div>
-            <div className={styles.familyRow}>
-                <div>
-                    <span className={styles.text}>{fatherFullName}</span>
-                    <span className={styles.text}> • </span>
-                    <span className={styles.text}>{motherFullName}</span>
-                </div>
-                <div>
-                    <span className={styles.text}>{relation}</span>
-                </div>
-            </div>
-
-            <VerticalSpacer size={32}/>
-            <div className={styles.familyRow}>
-                <div>
-                    <span className={styles.text}>{groomOrBrideStr}</span>
-                </div>
-                <div>
-                    <span className={styles.text}>{groomOrBrideFirstName}</span>
-                </div>
-            </div>
+          <span className={styles.text}>{fatherFullName}</span>
+          <span className={styles.text}> • </span>
+          <span className={styles.text}>{motherFullName}</span>
         </div>
-    )
+        <HorizontalSpacer size={8} />
+        <div>
+          <span className={styles.relationText}>{relation}</span>
+        </div>
+      </div>
+
+      <VerticalSpacer size={32} />
+      <div className={styles.familySecondRow}>
+        <div>
+          <span className={styles.relationText}>{groomOrBrideStr} </span>
+        </div>
+        <HorizontalSpacer size={8} />
+        <div>
+          <span className={styles.text}>{groomOrBrideFirstName}</span>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,10 +17,10 @@ export default function Home() {
         <Container>
           <TopContent />
           <MainMessage />
+          <FamilyInfos />
           <ViewPortDate />
           <Calendar />
           <Dday />
-          <FamilyInfos />
           <Gallery />
           <Location />
         </Container>
