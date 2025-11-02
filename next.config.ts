aef825670baa7de0bbd3b6773864898d25937da3
dEpayse <cbs5295@gmail.com>
2025-11-02 09:12:26 +0900

    feat. font 추가

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: '/wedding-invitation',
-  assetPrefix: '/wedding-invitation',
+  // basePath: '/wedding-invitation',
+  // assetPrefix: '/wedding-invitation',
 };
 
 export default nextConfig;
