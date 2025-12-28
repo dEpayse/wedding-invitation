185e0ff81eb297b03291c8d6833880ed5c716d87
depayse <cbs5295@gmail.com>
2025-12-28 17:54:05 +0900

    feat : 정보 영역 디테일 수정

--- src/app/components/information/Information.tsx
+++ src/app/components/information/Information.tsx
@@ -18,15 +18,16 @@ export default function Information() {
         <div className={styles.infoSection}>
           <div className={styles.infoItem}>
             <h3 className={styles.subtitle}>식사 안내</h3>
+            <VerticalSpacer size={8} />
             <p className={styles.description}>
               뷔페식으로 식사가 준비되어 있습니다.<br />
-              15:00 ~ 18:00까지 이용 가능합니다.
+              이용 가능한 시간은 15:00 ~ 18:00 입니다.
             </p>
             <a
               href="http://www.l65hotelwedding.co.kr/banquet/menu/"
               target="_blank"
               rel="noopener noreferrer"
-              className={styles.menuButton}
+              className={styles.menuLink}
             >
               메뉴 자세히 보기
             </a>
@@ -36,6 +37,7 @@ export default function Information() {
 
           <div className={styles.infoItem}>
             <h3 className={styles.subtitle}>화환 안내</h3>
+            <VerticalSpacer size={8} />
             <p className={styles.description}>
               축하의 마음만으로도 충분합니다.<br />
               정중히 화환은 사양합니다.
